9b48e171456d7496470ce9f1e84d74e6f89f0b2a
Nay <dnpd.dd@gmail.com>
2013-11-16 18:34:31 +0000

    Instead of requesting the searchResult page with ajax, incorporate it directly in searchTab.html

--- js/index.js
+++ js/index.js
@@ -42,12 +42,15 @@ function search() {
         case 'customer_field_search_list':
             field = $('#customer_field_search_select').val();
             doc = 'customer';
+            break;
         case 'product_field_search_list':
             field = $('#product_field_search_select').val();
             doc = 'product';
+            break;
         case 'invoice_field_search_list':
             field = $('#invoice_field_search_select').val();
             doc = 'invoice';
+            break;
     }
     
     var value1 = $('#field1').val();
@@ -58,17 +61,80 @@ function search() {
         getRequest = { 'doc': doc, 'op': op, 'field': field, 'value[]': value1 };
     else
         getRequest = { 'doc': doc, 'op': op, 'field': field, 'value[]': [value1, value2] };
-    
-    /* $.ajax({
-        url: 'searchResults.html',
-        type: 'GET',
-        async: false,
-        data: $.param(getRequest),
-        complete: function(jqXHR, textStatus) {
-            alert(jqXHR);
+
+    var arr = {
+        'invoice': {
+            'api': 'searchInvoicesByField.php',
+            'detailsHtml': 'showInvoice.html',
+            'printHtml': 'printInvoice.html',
+            'tableHeader': ['Number', 'Date', 'Client Code', 'Total Amount'],
+            'key': 'InvoiceNo',
+            'jsonFields': ['InvoiceNo', 'InvoiceDate', 'CustomerID', { 'DocumentTotals': 'GrossTotal' }],
+            'fields': ['InvoiceNo', 'InvoiceDate', 'CompanyName', 'GrossTotal']
+        },
+        'product': {
+            'api': 'searchProductsByField.php',
+            'detailsHtml': 'showProduct.html',
+            'hasPrint': false,
+            'tableHeader': ['Product Code'],
+            'key': 'ProductCode',
+            'jsonFields': ['ProductCode'],
+            'fields': ['ProductCode', 'ProductDescription']
+        },
+        'customer': {
+            'api': 'searchCustomersByField.php',
+            'detailsHtml': 'showCustomer.html',
+            'hasPrint': false,
+            'tableHeader': ['Customer Id', 'Company Name'],
+            'key': 'CustomerID',
+            'jsonFields': ['CustomerID', 'CompanyName'],
+            'fields': ['CustomerID', 'CustomerTaxID', 'CompanyName']
         }
-    }); */
-    
-    
-    $('#search_result').load('searchResults.html', decodeURI($.param(getRequest)));
+    }
+
+    var ops = ['range', 'equal', 'contains', 'min', 'max'];
+
+    $.getJSON('api/' + arr[doc].api, getRequest)
+        .done(function(data) {
+            
+            $('.search_results').empty();
+            $('.search_results').append('<table id="search_results_table"><tr id="header"></tr></table>');
+            
+            for (var i = 0; i < arr[doc].tableHeader.length; ++i)
+            {
+                $('#header').append('<th>' + arr[doc].tableHeader[i] + '</th>');   
+            }
+
+            $('#header').append('<th>Details</th>');
+            if (arr[doc].printHtml)
+                $('#header').append('<th>Print</th>');
+
+            for (var i = 0; i < data.length; ++i)
+            {
+                $('#search_results_table').append('<tr>');
+                for (var j = 0; j < arr[doc].jsonFields.length; ++j)
+                {
+                    if (typeof arr[doc].jsonFields[j] === 'object')
+                    {
+                        for (var key in arr[doc].jsonFields[j])
+                        {
+                            $('#search_results_table tbody').append('<td>' + data[i][key][arr[doc].jsonFields[j][key]] + '</td>');
+                        }
+                    }
+                    else
+                    {
+                        $('#search_results_table tbody').append('<td>' + data[i][arr[doc].jsonFields[j]] + '</td>');
+                    }
+                }
+                
+                $('#search_results_table tbody').append('<td><a target="_blank" href="' + arr[doc].detailsHtml + '?' + arr[doc].key + '=' + data[i][arr[doc].key] + '"><img src="images/icon_details.gif" title="more info" width="20" height="20"></a></td>');
+                
+                if (arr[doc].printHtml)
+                {
+                    $('#search_results_table tbody').append('<td><a target="_blank" href="' + arr[doc].printHtml + '?' + arr[doc].key + '=' + data[i][arr[doc].key] + '"><img src="images/icon_print.gif" title="print" width="20" height="20"></a></td>');
+                }
+                
+                $('#search_results_table').append('</tr>');
+            }
+        });
 }
