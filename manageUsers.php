432c286a3c9a7859a63c4a9e1a915ad6016e1f69
eduardo <edmartins_10@hotmail.com>
2013-12-03 16:28:12 +0000

    Create manageUsers.php

---
+++ manageUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'api/details/user_management.php';
+redirect_if_not_logged_in();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Online Invoicing System</title>
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/index.js"></script>
+    <script src="js/manageUsers.js"></script>
+    <link rel="stylesheet"  href="css/index.css" type="text/css">
+    <script type="text/javascript">
+        $(document).ready( function() {
+            load('search');
+            loadUsers();
+        });
+    </script>
+</head>
+<body>
+<div id="pageHeader"></div>
+
+<div class="search_results">
+    <table id="search_results_table">
+        <tr id="header">
+        </tr>
+    </table>
+</div>
+</body>
+</html>
