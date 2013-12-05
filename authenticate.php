e11ef3f632cc2015c653048d7adfe907430e8dcb
eduardo <edmartins_10@hotmail.com>
2013-12-05 17:51:57 +0000

    Update authentication.php

--- authenticate.php
+++ authenticate.php
@@ -20,13 +20,23 @@ if (is_logged_in())
     </head>
     <body>
     <div style="display: table; position: absolute; height: 100%; width: 100%">
-        <div style="display: table-cell; vertical-align: middle;">
-            <div style="margin-left: auto; margin-right: auto; width: 600px; padding-top: 110px; padding-bottom: 110px">
-                <form method="post" action="api/login.php" style="margin: 0 auto; width: 170px">
-                    <div style="margin-bottom: 10px"><label>Username: </label><br /><input type="text" name="username"/></div>
-                    <div style="margin-bottom: 20px"><label>Password: </label><br /><input type="password" name="password"/></div>
-                    <input type="submit" style=" width: 50px; margin-left: 55px;" value="Login"/>
+        <div style="position: relative; margin: 0 auto; top: 300px; height: 60%">
+            <div style="margin-left: auto; margin-right: auto; width: 300px; height:170px">
+                <div style="margin: 0 auto; width: 90%; height:70px">
+                    <h1 style="font-size: 35px; margin-bottom: 20px">OIS</h1>
+                </div>
+                <form method="post" action="api/login.php" style="margin: 0 auto; width: 90%; height:100%">
+                    <div style="margin-bottom: 10px"><label>Username</label><br /><input type="text" name="username" style="width: 250px"/></div>
+                    <div style="margin-bottom: 20px"><label>Password</label><br /><input type="password" name="password" style="width: 250px"/></div>
+                    <input type="submit" style="width: 100px; text-align: center" value="Login"/>
                 </form>
+                <div style="width: 90%; margin: 15px auto;">
+                    <ul>
+                        <li style="list-style-type: none">
+                            <a href="http://google.pt" style="text-decoration: none">Can't log in?</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
