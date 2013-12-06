06f9818f14e8b074d6297b5c8f6fad7ae7f49f68
eduardo <edmartins_10@hotmail.com>
2013-12-06 01:40:02 +0000

    Edit index.php

--- index.php
+++ index.php
@@ -12,18 +12,17 @@ redirect_if_not_logged_in();
         <title>Online Invoicing System</title>
         <script src="js/jquery-1.10.2.min.js"></script>
         <script src="js/index.js"></script>
-        <link rel="stylesheet"  href="css/index.css" type="text/css">
     </head>
     <body onload="load('index')">
         <div id="pageHeader"></div>
 
         <div class="_info_block">
             <h1>OIS</h1>
-            <pre><h3> Online Invoicing System </h3></pre>
-            <div class="homeTextBox">
-                <p> OIS is a free, open source, web based invoicing system that you can install on your server/pc. </p>
+            <pre><h3>    Online Invoicing System </h3></pre>
+            <div class="_text_block">
+                <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspOIS is a free, open source, web based invoicing system that you can install on your server/pc. </p>
                 
-                <p> This system provides a feature rich search API, allowing you to acess all the necessary information with ease. </p>
+                <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspThis system provides a feature rich search API, allowing you to acess all the necessary information with ease. </p>
             </div>
         </div>
     </body>
