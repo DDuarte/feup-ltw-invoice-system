b9b64d4735ab52eb3631d83d9f7dec895a2c8ed8
eduardo <edmartins_10@hotmail.com>
2013-12-06 01:35:19 +0000

    Format authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -16,28 +16,27 @@ if (is_logged_in())
         <meta charset="UTF-8" />
         <title>Online Invoicing System</title>
         <script src="js/jquery-1.10.2.min.js"></script>
-        <link rel="stylesheet"  href="css/index.css" type="text/css">
+        <link rel="stylesheet"  href="css/common.css" type="text/css">
+        <link rel="stylesheet"  href="css/authenticate.css" type="text/css">
     </head>
     <body>
-    <div style="display: table; position: absolute; height: 100%; width: 100%">
-        <div style="position: relative; margin: 0 auto; top: 300px; height: 60%">
-            <div style="margin-left: auto; margin-right: auto; width: 300px; height:170px">
-                <div style="margin: 0 auto; width: 90%; height:70px">
-                    <h1 style="font-size: 35px; margin-bottom: 20px">OIS</h1>
+    <div class="_login_box">
+            <div class="_login_form">
+                <div class="_form_title">
+                    <h1>OIS</h1>
                 </div>
-                <form method="post" action="api/login.php" style="margin: 0 auto; width: 90%; height:100%">
-                    <div style="margin-bottom: 10px"><label>Username</label><br /><input type="text" name="username" style="width: 250px"/></div>
-                    <div style="margin-bottom: 20px"><label>Password</label><br /><input type="password" name="password" style="width: 250px"/></div>
+                <form method="post" action="api/login.php">
+                    <div><label>Username</label><br /><input type="text" name="username"/></div>
+                    <div><label>Password</label><br /><input type="password" name="password"/></div>
                     <input type="submit" style="width: 100px; text-align: center" value="Login"/>
                 </form>
-                <div style="width: 90%; margin: 15px auto;">
+                <div class="_help">
                     <ul>
-                        <li style="list-style-type: none">
+                        <li>
                             <a href="http://google.pt" style="text-decoration: none">Can't log in?</a>
                         </li>
                     </ul>
                 </div>
-            </div>
         </div>
     </div>
     </body>
