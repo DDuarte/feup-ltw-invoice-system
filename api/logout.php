93dff3b59bbc081f20d38bad52527e5f6c933b47
migulorama <migulorama@gmail.com>
2013-12-08 13:42:12 +0000

    Fx slash in require_once in logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,5 +1,5 @@
 <?php
-    require_once 'details\user_management.php';
+    require_once 'details/user_management.php';
 
     if (session_status() !== PHP_SESSION_ACTIVE) {
         session_save_path(realpath($_SERVER['DOCUMENT_ROOT']) . '/sessions');
