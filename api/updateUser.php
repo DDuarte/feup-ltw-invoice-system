67d3ce6e9131a2e5dea2a42d13bda1f1e6c26bc6
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2013-12-08 17:55:13 +0000

    Add optional Role to updateUser.php.

--- api/updateUser.php
+++ api/updateUser.php
@@ -7,7 +7,7 @@ if (!is_logged_in())
     exit('{"error":{"code":403,"reason":"Not authenticated"}}');
 
 if (!is_editor())
-    exit('{"error":{"code":403,"reason":"No permission"}}');
+    exit('{"error":{"code":550,"reason":"Permission denied"}}');
 
 $error400 = '{"error":{"code":400,"reason":"Bad request"}}';
 
@@ -21,35 +21,60 @@ $user = json_decode($json, true);
 if (json_last_error() !== JSON_ERROR_NONE)
     exit($error400);
 
-if (!isset($user['username']) || !isset($user['password']))
+if (!isset($user['username']) || !isset($user['password']) || !isset($user['id']))
     exit($error400);
 
 $db = new PDO('sqlite:../sql/OIS.db');
 
-if (empty($user['id']))
-{
+if (empty($user['id'])) {
     $invoiceStmt = "INSERT INTO user(username, password, role_id) VALUES (:username, :password, :role_id);";
     $hasId = false;
-}
-else
-{
-    $invoiceStmt = "UPDATE OR FAIL user SET username = :username, password = :password, role_id = :role_id WHERE user.id = :userId;";
+    $hasPassword = true;
+    $hasRole = true;
+} else {
+    if (!empty($user['password'])) {
+        if (!empty($user['role_id']))
+            $invoiceStmt = "UPDATE OR FAIL user SET username = :username, password = :password, role_id = :role_id WHERE user.id = :userId;";
+        else {
+            $invoiceStmt = "UPDATE OR FAIL user SET username = :username, password = :password WHERE user.id = :userId;";
+            $hasRole = false;
+        }
+        $hasPassword = true;
+        $hasRole = true;
+    } else {
+        if (!empty($user['role_id']))
+            $invoiceStmt = "UPDATE OR FAIL user SET username = :username, role_id = :role_id WHERE user.id = :userId;";
+        else {
+            $invoiceStmt = "UPDATE OR FAIL user SET username = :username WHERE user.id = :userId;";
+            $hasRole = false;
+        }
+
+        $hasPassword = false;
+        $hasRole = true;
+    }
     $hasId = true;
 }
 $stmt = $db->prepare($invoiceStmt);
 
 if (!$stmt)
-    exit('Sorry, I dont know how to write sql statements');
+    exit($error400);
 
 $stmt->bindParam(':username', $user['username'], PDO::PARAM_STR);
-$hashedPassword = encrypt_credentials($user['username'], $user['password']);
-$stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
-$role_id = !isset($user['role_id']) ? 1 : $user['role_id'];
-$stmt->bindParam(':role_id', $role_id, PDO::PARAM_INT);
+
+if ($hasPassword) {
+    $hashedPassword = encrypt_credentials($user['username'], $user['password']);
+    $stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
+}
+
+if ($hasRole)
+    $stmt->bindParam(':role_id', $user['role_id'], PDO::PARAM_INT);
 
 if ($hasId)
     $stmt->bindParam(':userId', $user['id'], PDO::PARAM_INT);
 
 $stmt->execute();
 
-echo($user);
+if ($hasId)
+    echo $user['id'];
+else
+    echo $db->lastInsertId();
